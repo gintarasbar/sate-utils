945c7b2db2109d906f00b359854b23a51eb83747
gintaras <saganas@gmail.com>
2015-06-24 00:01:17 +0100

    fixing least square method

---
+++ src/main/java/edu/tfai/sate2/signal/ContinuumManager.java
@@ -0,0 +1,108 @@
+package edu.tfai.sate2.signal;
+
+import com.google.common.primitives.Doubles;
+import edu.tfai.sate2.spectra.Spectra;
+import edu.tfai.sate2.utils.LeastSquare;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction;
+import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static edu.tfai.sate2.synthetic.batch.BatchConstants.HI_RES_LIMIT;
+import static edu.tfai.sate2.signal.Smooth.getSmooth;
+import static edu.tfai.sate2.signal.Smooth.getSplineSmoothFunction;
+import static java.lang.String.format;
+
+@Slf4j
+public abstract class ContinuumManager {
+
+    public static double fixContinuum(Spectra originalSpectra, Spectra synthetic) {
+//        double mult1 = fixUsingPercentile(originalSpectra.copy(), synthetic);
+        double mult2 = fixUsingPlateMaxima(originalSpectra.copy(), synthetic);
+//
+//        if (mult1 < mult2) {
+//            log.info(format("Continuum fix1:%.4f", mult1));
+//            return mult1;
+//        }
+        log.info(format("Continuum fix2:%.4f", mult2));
+        return mult2;
+    }
+
+    private static double fixUsingPercentile(Spectra originalSpectra, Spectra synthetic) {
+
+        log.debug("Using smoothing for continuum: " + 20);
+
+        Spectra contSeries = getSmooth(originalSpectra, 20, 1);
+
+        if (originalSpectra.getResolution() > HI_RES_LIMIT) {
+            double yMax = contSeries.getYStats().getMax();
+            double yMaxSyn = synthetic.getYStats().getMax();
+
+            double median = contSeries.getYStats().getPercentile(75);
+            int size = contSeries.size();
+
+            List<Double> xx = new ArrayList(originalSpectra.size() / 2);
+            List<Double> yy = new ArrayList(originalSpectra.size() / 2);
+
+            for (int i = 0; i < size; i++) {
+                if (contSeries.getY(i) > median) {
+                    xx.add(contSeries.getX(i));
+                    yy.add(contSeries.getY(i));
+                }
+            }
+
+            Spectra sp = new Spectra(Doubles.toArray(xx), Doubles.toArray(yy));
+            median = (sp.getYStats().getPercentile(85) + yMax) / 2;
+
+            double mult = yMaxSyn / median;
+            log.debug("Range for continuum " + (contSeries.getMaxX() - contSeries.getMinX()));
+
+            return mult;
+        }
+        throw new IllegalStateException("Low resolution spectra detected on continuum fix: " + originalSpectra.getResolution());
+
+    }
+
+    private static double fixUsingPlateMaxima(Spectra originalSpectra, Spectra synthetic) {
+
+        int points = 3;
+        double[] maxPoints1 = getMaxPoints(getSmooth(originalSpectra, 1, 1), points);
+        double[] maxPoints2 = getMaxPoints(synthetic, points);
+
+        double multiplier = 0.0d;
+        SummaryStatistics summaryStatistics = new SummaryStatistics();
+        for (int i = 0; i < points; i++) {
+            double value = maxPoints2[i] / maxPoints1[i];
+            if(i==1) {
+                value = value + value * 10;
+            }
+            multiplier += value;
+//            System.out.println(maxPoints2[i] / maxPoints1[i]);
+        }
+        multiplier = multiplier / (points + 10);
+        return multiplier;
+    }
+
+    private static double[] getMaxPoints(Spectra spectra, int points) {
+
+        double levels[] = new double[points];
+
+        int totalPoints = points;//* 2 - 1;
+        int midP = (int) Math.floor(spectra.size() / totalPoints) - 1;
+        for (int i = 0; i < totalPoints; i += 1) {
+            Spectra sp = spectra.getSubSpectra(midP * i, midP * (i + 1));
+            levels[i] = sp.getYStats().getMax();
+        }
+        return levels;
+    }
+
+
+    public static double compareSpectraLevel(Spectra obsSpectra, Spectra synthetic) {
+        Spectra smoothedSpectra = getSmooth(obsSpectra, 20, 1);
+        double val = LeastSquare.squareDifferenceStDev(smoothedSpectra, synthetic);
+        return val;
+    }
+
+}
--- src/main/java/edu/tfai/sate2/utils/LeastSquare.java
+++ src/main/java/edu/tfai/sate2/utils/LeastSquare.java
@@ -2,43 +2,78 @@ package edu.tfai.sate2.utils;
 
 import edu.tfai.sate2.signal.Smooth;
 import edu.tfai.sate2.spectra.Spectra;
+import lombok.Data;
 import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction;
+import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
 
 import java.util.List;
 
 import static com.google.common.collect.Lists.newArrayList;
+import static java.lang.Math.abs;
 
 public class LeastSquare {
 
     public static double residuals(Spectra obsSpectra, Spectra syntheticSpectra) {
 
-        PolynomialSplineFunction function = Smooth.getSplineSmoothFunction(syntheticSpectra);
-        List<Double> xList = newArrayList();
-        List<Double> newSpectraList = newArrayList();
-        List<Double> observedSpectraList = newArrayList();
-
-
-        for (int i = 0; i < obsSpectra.size(); i++) {
-            double x = obsSpectra.getX(i);
-            if (syntheticSpectra.isInRange(x)) {
-                xList.add(x);
-                newSpectraList.add(function.value(x));
-                observedSpectraList.add(obsSpectra.getY(i));
-            }
-        }
+        SmoothedSpectra smoothedSpectra = new SmoothedSpectra(obsSpectra, syntheticSpectra).invoke();
+        List<Double> newObservedY = smoothedSpectra.getNewObservedSpectraYValues();
+        List<Double> newSyntheticY = smoothedSpectra.getNewSynthSpectraYValues();
 
 
         double sum = 0;
-        for (int i = 0; i < observedSpectraList.size(); i++) {
-            double y = newSpectraList.get(i);
-            double yObs = observedSpectraList.get(i);
-
-            sum += Math.pow(yObs - y, 2);
+        for (int i = 0; i < newObservedY.size(); i++) {
+            sum += Math.pow(newObservedY.get(i) - newSyntheticY.get(i), 2);
         }
 
         //TODO we could use https://commons.apache.org/proper/commons-math/javadocs/api-3.3/org/apache/commons/math3/stat/correlation/PearsonsCorrelation.html
         //http://alvinalexander.com/java/jwarehouse/commons-math/src/test/java/org/apache/commons/math/stat/correlation/PearsonsCorrelationTest.java.shtml
         //https://commons.apache.org/proper/commons-math/javadocs/api-3.4/org/apache/commons/math3/stat/inference/ChiSquareTest.html
         return sum;
+    }
+
+    public static double squareDifferenceStDev(Spectra obsSpectra, Spectra syntheticSpectra) {
+
+        SmoothedSpectra smoothedSpectra = new SmoothedSpectra(obsSpectra, syntheticSpectra).invoke();
+        List<Double> newObservedY = smoothedSpectra.getNewObservedSpectraYValues();
+        List<Double> newSyntheticY = smoothedSpectra.getNewSynthSpectraYValues();
+
+        SummaryStatistics stat = new SummaryStatistics();
+        for (int i = 0; i < newObservedY.size(); i++) {
+            stat.addValue(Math.pow(newSyntheticY.get(i) - newObservedY.get(i), 2));
+        }
+
+        return abs(stat.getStandardDeviation());
+    }
+
+    @Data
+    private static class SmoothedSpectra {
+        private final Spectra obsSpectra;
+        private final Spectra syntheticSpectra;
+        private List<Double> newSynthSpectraYValues;
+        private List<Double> newObservedSpectraYValues;
+        private List<Double> xValues;
+
+        public SmoothedSpectra invoke() {
+            if (obsSpectra.size() < 3 && syntheticSpectra.size() < 3) {
+                throw new IllegalArgumentException("Spectrum must have more than 2 points");
+            }
+
+            PolynomialSplineFunction function = Smooth.getSplineSmoothFunction(syntheticSpectra);
+            xValues = newArrayList();
+            newSynthSpectraYValues = newArrayList();
+            newObservedSpectraYValues = newArrayList();
+
+
+            for (int i = 0; i < obsSpectra.size(); i++) {
+                double x = obsSpectra.getX(i);
+                double y = obsSpectra.getY(i);
+                if (syntheticSpectra.isInRange(x) && y > 0.0001) {
+                    xValues.add(x);
+                    newSynthSpectraYValues.add(function.value(x));
+                    newObservedSpectraYValues.add(obsSpectra.getY(i));
+                }
+            }
+            return this;
+        }
     }
 }
